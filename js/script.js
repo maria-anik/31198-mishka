a91332cb79d00ce8a78241aeb57054febb8f20c6
Maria Anikina <maria-anik@mail.ru>
2017-06-13 19:37:48 +0300

    Четвертое задание v3.1 - Пробелы.2

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
 $(document).ready(function(){
-	$('.header_menu-btn').click(function(){
-		if($(this).hasClass('open')) {
-			$('.header_menu').slideUp();
-			$(this).removeClass('open');
-		}
-		else {
-			$('.header_menu').slideDown();
-			$(this).addClass('open');
-		}
-	});
+  $('.header_menu-btn').click(function(){
+    if($(this).hasClass('open')) {
+      $('.header_menu').slideUp();
+      $(this).removeClass('open');
+    }
+    else {
+      $('.header_menu').slideDown();
+      $(this).addClass('open');
+    }
+  });
 });
 
 $(window).resize(function(){
-	
+
 });
